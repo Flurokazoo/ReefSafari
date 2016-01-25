86e6b4ca95cdfff7e2c37ffe6b262618ad8016a5
Ifitskie <ifitskie@gmail.com>
2016-01-25 22:43:38 +0100

    nog meer css

--- ReefLogMobile/header.php
+++ ReefLogMobile/header.php
@@ -24,15 +24,12 @@ $currentPage = basename($_SERVER['PHP_SELF']);
 <div class="container">
     <section class="header-section">
         <div class="col-sm-4"></div>
+        <div class="col-sm-4"></div>
         <div class="col-sm-4">
             <h2 class="name white-text"><?= $user['firstName'] . " " . $user['lastName'] ?></h2>
-                <span class="yellow-text" id="settings-box"><span class="glyphicon glyphicon-cog"
-                                                                  aria-hidden="true"></span><span
-                        class="settings-box-glyphicon"><a href="settings.php"
-                                                          class="settings">Settings</a></span></span>
 
             <?php if ($currentPage != "menu.php") { ?>
-                <span class="yellow-text" id="settings-box"><span class="glyphicon glyphicon-camera"
+                <span class="yellow-text" id="settings-box"><span class="glyphicon glyphicon-home"
                                                                   aria-hidden="true"></span><span
                         class="settings-box-glyphicon"><a href="menu.php"
                                                           class="settings">Go to Menu</a></span></span>
@@ -43,28 +40,6 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                     class="settings-box-glyphicon"><a href="logout.php"
                                                       class="">Logout</a></span></span>
         </div>
-<!--        <div class="col-lg-6">-->
-<!--            <div class="col-sm-10 user-options">-->
-<!--                <h2 class="name white-text">--><?//= $user['firstName'] . " " . $user['lastName'] ?><!--</h2>-->
-<!--                <span class="yellow-text" id="settings-box"><span class="glyphicon glyphicon-cog"-->
-<!--                                                                  aria-hidden="true"></span><span-->
-<!--                        class="settings-box-glyphicon"><a href="settings.php"-->
-<!--                                                          class="settings">Settings</a></span></span>-->
-<!---->
-<!--                --><?php //if ($currentPage != "menu.php") { ?>
-<!--                    <span class="yellow-text" id="settings-box"><span class="glyphicon glyphicon-camera"-->
-<!--                                                                      aria-hidden="true"></span><span-->
-<!--                            class="settings-box-glyphicon"><a href="menu.php"-->
-<!--                                                              class="settings">Go to Menu</a></span></span>-->
-<!--                --><?php //} ?>
-<!---->
-<!--                <span class="yellow-text" id="logout-box"><span class="glyphicon glyphicon-log-out"-->
-<!--                                                                aria-hidden="true"></span><span-->
-<!--                        class="settings-box-glyphicon"><a href="logout.php"-->
-<!--                                                          class="">Logout</a></span></span>-->
-<!--            </div>-->
-
-<!--        </div>-->
     </section>
 
 
--- ReefLogMobile/menu.php
+++ ReefLogMobile/menu.php
@@ -15,22 +15,22 @@ if (count($resultArray) > 0) {
 }
 
 ?>
-<div class="col-sm-4"></div>
-<!--<div class="col-sm-4"></div>-->
-<div id="reeflog-logo" class="col-sm-4">
+
+<div id="reeflog-logo">
     <h1><span class="yellow-text">Reef</span><span
             class="white-text italic-text">Safari</span></h1>
 </div>
 <section class="body-section">
+    <p id="status-box"></p>
     <input type="file" id="take-picture" accept="image/jpeg" class="btn btn-success login border" placeholder="Upload/Take a picture">
     <label for="take-picture" class="btn btn-success login border">Upload/Take a Picture </label>
 
     <a href="reeflog.php" class="btn btn-success login border">View ReefLog</a>
     <?php if ($showReview) { ?>
-        <a href="review.php" class="btn btn-success login border">View Revlog</a>
+        <a href="review.php" class="btn btn-success login border">Review Corals</a>
     <?php } ?>
 <!--    <img src="about:blank" alt="" id="show-picture">-->
-    <p id="status-box"></p>
+
 </section>
 
 <?php require_once "footer.php" ?>
